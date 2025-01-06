dba2a2005743d08468b1b7bed9e95e5b520b7578
ken7253 <ken7253@gmail.com>
2025-01-07 00:02:08 +0900

    Create a class for information registration.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,31 @@
-export const Sample = "sample";
+type RegisterItem = {
+  action: () => void;
+};
+
+type Register = Record<string, RegisterItem> | null;
+
+export class KeyShortcut {
+  #register: Register = null;
+
+  add(key: string[], action: () => void) {
+    const registerKey = key.join("+");
+    const item: RegisterItem = {
+      action,
+    };
+
+    this.#register = {
+      ...this.#register,
+      [registerKey]: item,
+    };
+  }
+
+  remove(key: string[]) {
+    const registerKey = key.join("+");
+
+    if (this.#register == null) return;
+
+    if (Object.hasOwn(this.#register, registerKey)) {
+      delete this.#register[registerKey];
+    }
+  }
+}
