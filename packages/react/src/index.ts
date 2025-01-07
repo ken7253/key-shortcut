5c865004911a670061c793df029b57d0120132e3
ken7253 <ken7253@gmail.com>
2025-01-07 16:46:56 +0900

    Implemented registration process via Hooks.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,10 @@
-import {} from "@key-shortcut/core";
+import { KeyShortcut } from "@key-shortcut/core";
+import { useEffect } from "react";
+
+export const useShortcut = (key: string[], action: () => void) => {
+  useEffect(() => {
+    KeyShortcut.add(key, action);
+
+    return () => KeyShortcut.remove(key);
+  }, [action, key]);
+};
